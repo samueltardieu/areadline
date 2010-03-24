99be20ef21122d215cd923d02df96deb46ec47f3
Julien Brette <julien.brette@polytechnique.org>
2010-03-24 22:28:48 +0100

    Add a custom completer

--- read_line.ads
+++ read_line.ads
@@ -1,2 +1,14 @@
-function Read_Line (Prompt : String := "") return String;
---  May raise Ada.IO_Exceptions.End_Error if end-of-file is encountered
+package Read_Line is
+
+   procedure Add_Word (Word : String);
+
+   procedure Clear_All_Words;
+
+   procedure Variable_Bind (Name : String; Value : String);
+
+   function Variable_Value (Name : String) return String;
+
+   function Read_Line (Prompt : String := "") return String;
+   --  May raise Ada.IO_Exceptions.End_Error if end-of-file is encountered
+
+end Read_Line;
--- test_read_line.adb
+++ test_read_line.adb
@@ -3,9 +3,13 @@ with Read_Line;
 
 procedure Test_Read_Line is
 begin
+   Read_Line.Add_Word ("One");
+   Read_Line.Add_Word ("Two");
+   Read_Line.Add_Word ("Three");
+   Read_Line.Add_Word ("Four");
    loop
       declare
-         Line : constant String := Read_Line ("Type something> ");
+         Line : constant String := Read_Line.Read_Line ("Type something> ");
       begin
          Put_Line ("You typed: " & Line);
       end;
